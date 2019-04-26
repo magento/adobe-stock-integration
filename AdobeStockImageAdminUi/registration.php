e2495ed3b1fe77cedd7c7bf96afaf457e21d84a4
Stanislav Idolov <sidolov@magento.com>
2019-04-26 13:00:06 -0500

    Fixed module registration

--- AdobeStockImageAdminUi/registration.php
+++ AdobeStockImageAdminUi/registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'AdobeStockImageAdminUi',
+    'Magento_AdobeStockImageAdminUi',
     __DIR__
 );
