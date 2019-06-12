084dbc3388415042b64220e4b6b305f42b5b327a
Sergii Ivashchenko <serg.ivashchenko@gmail.com>
2019-06-12 10:56:42 +0100

    magento/adobe-stock-integration#79: Removed unnecessary spaces and enforced strict types

--- AdobeStockImageAdminUi/Ui/Component/Listing/Columns/Isolated/Options.php
+++ AdobeStockImageAdminUi/Ui/Component/Listing/Columns/Isolated/Options.php
@@ -3,7 +3,6 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-
 declare(strict_types=1);
 
 namespace Magento\AdobeStockImageAdminUi\Ui\Component\Listing\Columns\Isolated;
@@ -13,13 +12,12 @@ namespace Magento\AdobeStockImageAdminUi\Ui\Component\Listing\Columns\Isolated;
  */
 class Options implements \Magento\Framework\Data\OptionSourceInterface
 {
-
     /**
      * Get options
      *
      * @return array
      */
-    public function toOptionArray()
+    public function toOptionArray(): array
     {
         return [['value' => 1, 'label' => __('Yes')], ['value' => 0, 'label' => __('No')]];
     }
