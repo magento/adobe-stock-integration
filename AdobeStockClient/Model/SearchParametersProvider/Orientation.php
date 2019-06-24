2ccd114115bce9e660daf54e38c66ced77c3a904
Stanislav Idolov <sidolov@magento.com>
2019-06-24 12:11:34 -0500

    Added Panoramic options to the Orientation filter

---
+++ AdobeStockClient/Model/SearchParametersProvider/Orientation.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\AdobeStockClient\Model\SearchParametersProvider;
+
+use AdobeStock\Api\Models\SearchParameters;
+use Magento\AdobeStockClientApi\Api\SearchParameterProviderInterface;
+use Magento\Framework\Api\SearchCriteriaInterface;
+
+class Orientation implements SearchParameterProviderInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function apply(SearchCriteriaInterface $searchCriteria, SearchParameters $searchParams): SearchParameters
+    {
+        foreach ($searchCriteria->getFilterGroups() as $filterGroup) {
+            foreach ($filterGroup->getFilters() as $filter) {
+                if ($filter->getField() === 'orientation_filter') {
+                    if ($filter->getValue() === 'PANORAMIC') {
+                        $searchParams->setFilterPanoromicOn(true);
+                        break;
+                    }
+
+                    $searchParams->setOrientation($filter->getValue());
+                    break;
+                }
+            }
+        }
+        return $searchParams;
+    }
+}
