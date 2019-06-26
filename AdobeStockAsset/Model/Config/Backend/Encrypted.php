724c31d2af785f79c14f7b358c10c8f83297ef82
Alexander Shkurko <coderimus@gmail.com>
2019-06-26 07:46:40 +0300

    Extend save api key functionality

---
+++ AdobeStockAsset/Model/Config/Backend/Encrypted.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\AdobeStockAsset\Model\Config\Backend;
+
+use Magento\Framework\Model\Context;
+use Magento\Framework\Registry;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\Cache\TypeListInterface;
+use Magento\Framework\Encryption\EncryptorInterface;
+use Magento\Framework\Model\ResourceModel\AbstractResource;
+use Magento\Framework\Data\Collection\AbstractDb;
+
+/**
+ * Class Encrypted
+ */
+class Encrypted extends \Magento\Config\Model\Config\Backend\Encrypted
+{
+    /**
+     * Encrypted constructor.
+     *
+     * @param Context               $context
+     * @param Registry              $registry
+     * @param ScopeConfigInterface  $config
+     * @param TypeListInterface     $cacheTypeList
+     * @param EncryptorInterface    $encryptor
+     * @param AbstractResource|null $resource
+     * @param AbstractDb|null       $resourceCollection
+     * @param array                 $data
+     */
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        ScopeConfigInterface $config,
+        TypeListInterface $cacheTypeList,
+        EncryptorInterface $encryptor,
+        AbstractResource $resource = null,
+        AbstractDb $resourceCollection = null,
+        array $data = []
+    ) {
+        parent::__construct(
+            $context,
+            $registry,
+            $config,
+            $cacheTypeList,
+            $encryptor,
+            $resource,
+            $resourceCollection,
+            $data
+        );
+    }
+
+    /**
+     * Encrypt value before saving
+     *
+     * @return void
+     */
+    public function beforeSave()
+    {
+        $this->_dataSaveAllowed = false;
+        $value = (string)$this->getValue();
+        if ($this->isAPiKeyValid($value)) {
+            parent::beforeSave();
+        }
+    }
+
+    /**
+     * @param $value
+     *
+     * @return bool
+     */
+    private function isAPiKeyValid($value): bool
+    {
+        
+    }
+}
