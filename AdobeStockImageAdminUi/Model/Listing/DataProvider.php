99f0dd27c1342cb253796cc897977f9fe6eb3b98
Raul E Watson <diazwatson@gmail.com>
2019-08-13 01:52:04 +0100

    #285 Remove unnecessary conversions of asset when loading it from API

--- AdobeStockImageAdminUi/Model/Listing/DataProvider.php
+++ AdobeStockImageAdminUi/Model/Listing/DataProvider.php
@@ -8,18 +8,55 @@ declare(strict_types=1);
 
 namespace Magento\AdobeStockImageAdminUi\Model\Listing;
 
+use Magento\Framework\Api\Filter;
 use Magento\Framework\Api\FilterBuilder;
-use Magento\Framework\Api\Search\ReportingInterface;
+use Magento\Framework\Api\Search\SearchCriteria;
 use Magento\Framework\Api\Search\SearchCriteriaBuilder;
+use Magento\Framework\Api\Search\SearchResultInterface;
 use Magento\Framework\App\RequestInterface;
 use Magento\AdobeStockImageApi\Api\GetImageListInterface;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\View\Element\UiComponent\DataProvider\DataProviderInterface;
 use Magento\Ui\DataProvider\SearchResultFactory;
 
 /**
  * DataProvider of customer addresses for customer address grid.
  */
-class DataProvider extends \Magento\Framework\View\Element\UiComponent\DataProvider\DataProvider
+class DataProvider implements DataProviderInterface
 {
+    /**
+     * Data Provider name
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * Data Provider Primary Identifier name
+     *
+     * @var string
+     */
+    protected $primaryFieldName;
+
+    /**
+     * Data Provider Request Parameter Identifier name
+     *
+     * @var string
+     */
+    protected $requestFieldName;
+
+    /**
+     * Provider configuration data
+     *
+     * @var array
+     */
+    protected $data = [];
+
+    /**
+     * @var array
+     */
+    protected $meta = [];
+
     /**
      * @var GetImageListInterface
      */
@@ -31,24 +68,42 @@ class DataProvider extends \Magento\Framework\View\Element\UiComponent\DataProvi
     private $searchResultFactory;
 
     /**
+     * @var SearchCriteria
+     */
+    protected $searchCriteria;
+
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    private $searchCriteriaBuilder;
+
+    /**
+     * @var FilterBuilder
+     */
+    private $filterBuilder;
+
+    /**
+     * @var RequestInterface
+     */
+    private $request;
+
+    /**
      * DataProvider constructor.
-     * @param string $name
-     * @param string $primaryFieldName
-     * @param string $requestFieldName
-     * @param ReportingInterface $reporting
+     * @param string                $name
+     * @param string                $primaryFieldName
+     * @param string                $requestFieldName
      * @param SearchCriteriaBuilder $searchCriteriaBuilder
-     * @param RequestInterface $request
-     * @param FilterBuilder $filterBuilder
+     * @param RequestInterface      $request
+     * @param FilterBuilder         $filterBuilder
      * @param GetImageListInterface $getImageList
-     * @param SearchResultFactory $searchResultFactory
-     * @param array $meta
-     * @param array $data
+     * @param SearchResultFactory   $searchResultFactory
+     * @param array                 $meta
+     * @param array                 $data
      */
     public function __construct(
         string $name,
         string $primaryFieldName,
         string $requestFieldName,
-        ReportingInterface $reporting,
         SearchCriteriaBuilder $searchCriteriaBuilder,
         RequestInterface $request,
         FilterBuilder $filterBuilder,
@@ -57,23 +112,51 @@ class DataProvider extends \Magento\Framework\View\Element\UiComponent\DataProvi
         array $meta = [],
         array $data = []
     ) {
-        parent::__construct(
-            $name,
-            $primaryFieldName,
-            $requestFieldName,
-            $reporting,
-            $searchCriteriaBuilder,
-            $request,
-            $filterBuilder,
-            $meta,
-            $data
-        );
+        $this->name = $name;
+        $this->primaryFieldName = $primaryFieldName;
+        $this->requestFieldName = $requestFieldName;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->request = $request;
+        $this->filterBuilder = $filterBuilder;
+        $this->meta = $meta;
+        $this->data = $data;
         $this->getImageList = $getImageList;
         $this->searchResultFactory = $searchResultFactory;
+        $this->prepareUpdateUrl();
+    }
+
+    /**
+     * Prepare UpdateUrl
+     *
+     * @return void
+     */
+    protected function prepareUpdateUrl()
+    {
+        if (!isset($this->data['config']['filter_url_params'])) {
+            return;
+        }
+        foreach ($this->data['config']['filter_url_params'] as $paramName => $paramValue) {
+            if ('*' === $paramValue) {
+                $paramValue = $this->request->getParam($paramName);
+            }
+            if ($paramValue) {
+                $this->data['config']['update_url'] = sprintf(
+                    '%s%s/%s/',
+                    $this->data['config']['update_url'],
+                    $paramName,
+                    $paramValue
+                );
+                $this->addFilter(
+                    $this->filterBuilder->setField($paramName)->setValue($paramValue)->setConditionType('eq')->create()
+                );
+            }
+        }
     }
 
     /**
      * @inheritdoc
+     *
+     * @throws LocalizedException
      */
     public function getSearchResult()
     {
@@ -84,5 +167,148 @@ class DataProvider extends \Magento\Framework\View\Element\UiComponent\DataProvi
             $this->getSearchCriteria(),
             'id'
         );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getConfigData()
+    {
+        return $this->data['config'] ?? [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setConfigData($config)
+    {
+        $this->data['config'] = $config;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getMeta()
+    {
+        return $this->meta;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getFieldMetaInfo($fieldSetName, $fieldName)
+    {
+        return $this->meta[$fieldSetName]['children'][$fieldName] ?? [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getFieldSetMetaInfo($fieldSetName)
+    {
+        return $this->meta[$fieldSetName] ?? [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getFieldsMetaInfo($fieldSetName)
+    {
+        return $this->meta[$fieldSetName]['children'] ?? [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getPrimaryFieldName()
+    {
+        return $this->primaryFieldName;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getRequestFieldName()
+    {
+        return $this->requestFieldName;
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @throws LocalizedException
+     */
+    public function getData()
+    {
+        return $this->searchResultToOutput($this->getSearchResult());
+    }
+
+    /**
+     * Search Result To OutPut
+     *
+     * @param SearchResultInterface $searchResult
+     * @return array
+     */
+    protected function searchResultToOutput(SearchResultInterface $searchResult)
+    {
+        $arrItems = [];
+
+        $arrItems['items'] = [];
+        foreach ($searchResult->getItems() as $item) {
+            $itemData = [];
+            foreach ($item->getCustomAttributes() as $attribute) {
+                $itemData[$attribute->getAttributeCode()] = $attribute->getValue();
+            }
+            $arrItems['items'][] = $itemData;
+        }
+
+        $arrItems['totalRecords'] = $searchResult->getTotalCount();
+
+        return $arrItems;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function addFilter(Filter $filter)
+    {
+        $this->searchCriteriaBuilder->addFilter($filter);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function addOrder($field, $direction)
+    {
+        $this->searchCriteriaBuilder->addSortOrder($field, $direction);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setLimit($offset, $size)
+    {
+        $this->searchCriteriaBuilder->setPageSize($size);
+        $this->searchCriteriaBuilder->setCurrentPage($offset);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getSearchCriteria()
+    {
+        if (!$this->searchCriteria) {
+            $this->searchCriteria = $this->searchCriteriaBuilder->create();
+            $this->searchCriteria->setRequestName($this->name);
+        }
+        return $this->searchCriteria;
     }
 }
