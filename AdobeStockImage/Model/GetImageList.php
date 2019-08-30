30dc4a041f91fb812b3d267c544ca5aa27d5aefc
Nazarn96 <nazarn96@gmail.com>
2019-08-30 14:25:29 +0300

    Merge develop into  ASI-2833

--- AdobeStockImage/Model/GetImageList.php
+++ AdobeStockImage/Model/GetImageList.php
@@ -12,6 +12,8 @@ use Magento\AdobeStockAssetApi\Api\GetAssetListInterface;
 use Magento\AdobeStockImageApi\Api\GetImageListInterface;
 use Magento\Framework\Api\Search\SearchResultInterface;
 use Magento\Framework\Api\Search\SearchCriteriaInterface;
+use Magento\Framework\Api\Search\FilterGroupBuilder;
+use Magento\Framework\Api\FilterBuilder;
 
 /**
  * Class GetImageList
@@ -24,14 +26,38 @@ class GetImageList implements GetImageListInterface
     private $getAssetList;
 
     /**
+     * @var array $defaultFilter
+     */
+    private $defaultFilters;
+
+    /**
+     * @var FilterGroupBuilder
+     */
+    private $filterGroupBuilder;
+
+    /**
+     * @var FilterBuilder
+     */
+    private $filterBuilder;
+
+    /**
      * GetImageList constructor.
      *
+     * @param FilterGroupBuilder $filterGroupBuilder
      * @param GetAssetListInterface $getAssetList
+     * @param FilterBuilder $filterBuilder
+     * @param array $defaultFilters
      */
     public function __construct(
-        GetAssetListInterface $getAssetList
+        FilterGroupBuilder $filterGroupBuilder,
+        GetAssetListInterface $getAssetList,
+        FilterBuilder $filterBuilder,
+        array $defaultFilters = []
     ) {
         $this->getAssetList = $getAssetList;
+        $this->filterGroupBuilder = $filterGroupBuilder;
+        $this->defaultFilters = $defaultFilters;
+        $this->filterBuilder = $filterBuilder;
     }
 
     /**
@@ -51,8 +77,16 @@ class GetImageList implements GetImageListInterface
      */
     private function setDefaultFilters(SearchCriteriaInterface $searchCriteria)
     {
-        if (!$searchCriteria->getFilterGroups()) {
-            $filters = [];
+        $isContentTypeFilter = false;
+        $filters = [];
+        foreach ($searchCriteria->getFilterGroups() as $filterGroup) {
+            foreach ($filterGroup->getFilters() as $filter) {
+                if ($filter->getField() === 'content_type_filter') {
+                    $isContentTypeFilter = true;
+                }
+            }
+        }
+        if (!$isContentTypeFilter) {
             foreach ($this->defaultFilters as $filter) {
                 $filters[] = $this->filterBuilder
                     ->setField($filter['type'])
