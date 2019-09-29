620c57729f729d8073f6526309c89d14d50e478e
Shankar Konar <konar.shankar2013@gmail.com>
2019-09-29 11:26:21 +0530

    Fixed Static build test

--- AdobeIms/Test/Unit/Model/LogOutTest.php
+++ AdobeIms/Test/Unit/Model/LogOutTest.php
@@ -6,7 +6,7 @@
 
 declare(strict_types=1);
 
-namespace Magento\AdobeIms\Test\Unit\Controller\Adminhtml\User;
+namespace Magento\AdobeIms\Test\Unit\Model;
 
 use Magento\AdobeIms\Model\LogOut;
 use PHPUnit\Framework\{MockObject\MockObject, TestCase};
