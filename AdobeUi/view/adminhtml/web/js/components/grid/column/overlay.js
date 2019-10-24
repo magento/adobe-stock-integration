2c0aad0d41646d55574c2906fd2710af951d52d4
Shankar Konar <konar.shankar2013@gmail.com>
2019-10-24 23:58:47 +0530

    styling licensed overlay label changes

--- AdobeUi/view/adminhtml/web/js/components/grid/column/overlay.js
+++ AdobeUi/view/adminhtml/web/js/components/grid/column/overlay.js
@@ -3,11 +3,44 @@
  * See COPYING.txt for license details.
  */
 define([
-    'Magento_Ui/js/grid/columns/column'
-], function (Column) {
+    'Magento_Ui/js/grid/columns/column',
+    'ko'
+], function (Column, ko) {
     'use strict';
 
     return Column.extend({
+
+        /**
+         * Returns container styles to given record.
+         *
+         * @param {Object} record - Data to be preprocessed.
+         * @returns {Object}
+         */
+        getStyles: function (record) {
+            var styles, overlayStyles, marginTop, marginLeft;
+            if (!record.overlayStyles) {
+                record.overlayStyles = ko.observable([]);
+            }
+            if (record.styles) {
+                styles = record.styles();
+                overlayStyles = record.overlayStyles();
+                marginTop =  styles.height.split('px').map(function(pixel){
+                    if (pixel) {
+                        return pixel - 50;
+                    }
+                }).join('px');
+                marginLeft =  styles.width.split('px').map(function(pixel){
+                    if (pixel) {
+                        return pixel - 80;
+                    }
+                }).join('px');
+                overlayStyles['margin-top'] = marginTop;
+                overlayStyles['margin-left'] = marginLeft;
+                return overlayStyles;
+            }
+
+            return {};
+        },
         /**
          * If overlay should be visible
          *
