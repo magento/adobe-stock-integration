6da9250cd407223a7a5fc4bac3f088ecd5f2371d
Sergii Ivashchenko <serg.ivashchenko@gmail.com>
2019-11-01 23:15:15 +0000

    magento/adobe-stock-integration#581: Adjusted the warning text

--- AdobeStockImageAdminUi/view/adminhtml/web/js/media-gallery.js
+++ AdobeStockImageAdminUi/view/adminhtml/web/js/media-gallery.js
@@ -67,8 +67,8 @@ define([
                     locatedImage.click();
                 } else {
                     confirm({
-                        title: $.mage.__('Image location error'),
-                        content: $.mage.__('The image cannot be located!'),
+                        title: $.mage.__('The image cannot be located'),
+                        content: $.mage.__('We cannot find this image in the media gallery.'),
                         buttons: [{
                             text: $.mage.__('Okay'),
                             class: 'action-primary',
