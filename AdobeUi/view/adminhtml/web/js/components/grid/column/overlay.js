070deb841185398d3cd27c8a7056bbdc7ddec38a
Shankar Konar <konar.shankar2013@gmail.com>
2019-11-02 12:41:36 +0530

    License label styling using css

--- AdobeUi/view/adminhtml/web/js/components/grid/column/overlay.js
+++ AdobeUi/view/adminhtml/web/js/components/grid/column/overlay.js
@@ -9,38 +9,6 @@ define([
     'use strict';
 
     return Column.extend({
-
-        /**
-         * Returns container styles to given record.
-         *
-         * @param {Object} record - Data to be preprocessed.
-         * @returns {Object}
-         */
-        getStyles: function (record) {
-            var styles, overlayStyles, marginTop, marginLeft;
-            if (!record.overlayStyles) {
-                record.overlayStyles = ko.observable([]);
-            }
-            if (record.styles) {
-                styles = record.styles();
-                overlayStyles = record.overlayStyles();
-                marginTop =  styles.height.split('px').map(function(pixel){
-                    if (pixel) {
-                        return pixel - 50;
-                    }
-                }).join('px');
-                marginLeft =  styles.width.split('px').map(function(pixel){
-                    if (pixel) {
-                        return pixel - 80;
-                    }
-                }).join('px');
-                overlayStyles['margin-top'] = marginTop;
-                overlayStyles['margin-left'] = marginLeft;
-                return overlayStyles;
-            }
-
-            return {};
-        },
         /**
          * If overlay should be visible
          *
