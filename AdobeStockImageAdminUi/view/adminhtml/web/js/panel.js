d68acfd843b949f7da1fe56a5a52ebc8cae95736
Adarsh Manickam <amanickam@ztech.io>
2019-11-14 02:43:48 +0530

    Resolved issue 691

--- AdobeStockImageAdminUi/view/adminhtml/web/js/panel.js
+++ AdobeStockImageAdminUi/view/adminhtml/web/js/panel.js
@@ -25,6 +25,11 @@ define([
          * @return {exports}
          */
         initialize: function () {
+            var imageIndex, startIndex, endIndex,
+                imagePreviewSelector = '.masonry-image-preview',
+                imageColumnSelector = '.masonry-image-column',
+                adobeModalSelector = '.adobe-stock-modal';
+
             this._super();
 
             $(this.containerId).modal({
@@ -35,6 +40,25 @@ define([
             }).on('openModal', function () {
                 this.masonry().setLayoutStylesWhenLoaded();
             }.bind(this)).applyBindings();
+
+            $(document).on('keydown', function(e) {
+                startIndex = 0;
+                endIndex = $('.masonry-image-grid')[0].children.length - 1;
+
+                if($(imagePreviewSelector).length > 0) {
+                    imageIndex = $(imagePreviewSelector)
+                        .parents(imageColumnSelector)
+                        .data('repeatIndex');
+                }
+
+                if($(adobeModalSelector).hasClass('_show')) {
+                    if(e.keyCode === 37 && imageIndex !== startIndex) {
+                        $(imagePreviewSelector + ' .action-previous').click();
+                    } else if (e.keyCode === 39 && imageIndex !== endIndex) {
+                        $(imagePreviewSelector + ' .action-next').click();
+                    }
+                }
+            });
 
             return this;
         }
