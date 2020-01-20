8a5e96c7cad3f08870ca0e0faeaa2f9616197139
Sergii Ivashchenko <serg.ivashchenko@gmail.com>
2020-01-20 20:31:13 +0000

    Moved media gallery to a slide panel

---
+++ MediaGalleryUi/Plugin/UpdateOpenDialogUrl.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\MediaGalleryUi\Plugin;
+
+use Magento\Framework\UrlInterface;
+use Magento\Ui\Component\Form\Element\DataType\Media\Image;
+use Magento\MediaGalleryUiApi\Api\ConfigInterface;
+
+/**
+ * Plugin to update open media gallery dialog URL for image-uploader component
+ */
+class UpdateOpenDialogUrl
+{
+    /**
+     * @var UrlInterface
+     */
+    private $url;
+
+    /**
+     * @var ConfigInterface
+     */
+    private $config;
+
+    /**
+     * @param UrlInterface $url
+     */
+    public function __construct(UrlInterface $url, ConfigInterface $config)
+    {
+        $this->url = $url;
+        $this->config = $config;
+    }
+
+    /**
+     * Update open media gallery dialog URL for image-uploader component
+     *
+     * @param Image $component
+     */
+    public function afterPrepare(Image $component): void
+    {
+        if (!$this->config->isEnabled()) {
+            return;
+        }
+
+        $data = $component->getData();
+
+        $data['config']['mediaGallery']['openDialogUrl'] = $this->url->getUrl(
+            'media_gallery/index/index',
+            ['_secure' => true]
+        );
+
+        $component->setData($data);
+    }
+}
---
+++ MediaGalleryUi/view/adminhtml/web/js/container.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'uiElement',
+    'jquery',
+    'mage/translate'
+], function (Element, $, $t) {
+    'use strict';
+
+    return Element.extend({
+        defaults: {
+            containerSelector: '.media-gallery-container',
+            masonryComponentPath: 'media_gallery_listing.media_gallery_listing.media_gallery_columns',
+            modules: {
+                masonry: '${ $.masonryComponentPath }'
+            }
+        },
+
+        /**
+         * Init component
+         *
+         * @return {exports}
+         */
+        initialize: function () {
+            this._super();
+
+            $(this.containerSelector).applyBindings();
+            setTimeout(function () {
+                this.masonry().setLayoutStylesWhenLoaded();
+            }.bind(this), 2000);
+
+            return this;
+        }
+    });
+});
