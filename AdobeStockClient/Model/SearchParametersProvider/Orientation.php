113baa3c882e35cd7b50920d068931d37f22a1e6
Rakesh  Jesadiya <jesadiya.rakesh@gmail.com>
2020-01-24 17:57:41 +0530

    Define Constant Value
    Define Constant Value

--- AdobeStockClient/Model/SearchParametersProvider/Orientation.php
+++ AdobeStockClient/Model/SearchParametersProvider/Orientation.php
@@ -9,14 +9,17 @@ declare(strict_types=1);
 namespace Magento\AdobeStockClient\Model\SearchParametersProvider;
 
 use AdobeStock\Api\Models\SearchParameters;
-use Magento\AdobeStockClient\Model\SearchParameterProviderInterface;
 use Magento\Framework\Api\SearchCriteriaInterface;
+use Magento\AdobeStockClient\Model\SearchParameterProviderInterface;
 
 /**
  * Filter for images orientation: landscape, square, vertical, etc.
  */
 class Orientation implements SearchParameterProviderInterface
 {
+    private const ORIENTATION_FILTER = 'orientation_filter';
+    private const PANORAMIC = 'PANORAMIC';
+    
     /**
      * @inheritdoc
      */
@@ -24,8 +27,8 @@ class Orientation implements SearchParameterProviderInterface
     {
         foreach ($searchCriteria->getFilterGroups() as $filterGroup) {
             foreach ($filterGroup->getFilters() as $filter) {
-                if ($filter->getField() === 'orientation_filter') {
-                    if ($filter->getValue() === 'PANORAMIC') {
+                if ($filter->getField() === self::ORIENTATION_FILTER) {
+                    if ($filter->getValue() === self::PANORAMIC) {
                         $searchParams->setFilterPanoromicOn(true);
                         break;
                     }
