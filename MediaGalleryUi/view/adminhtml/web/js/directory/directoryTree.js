2ee47194a299b15c79e54fab3f104b64291eff82
Nazar Klovanych <nazarn96@gmail.com>
2020-01-26 20:46:42 +0200

    Refactor to use original jstree plugin

--- MediaGalleryUi/view/adminhtml/web/js/directory/directoryTree.js
+++ MediaGalleryUi/view/adminhtml/web/js/directory/directoryTree.js
@@ -2,48 +2,86 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-	
+
 define([
     'jquery',
     'uiElement',
-    'Magento_Cms/js/folder-tree'
+    'jquery/jstree/jquery.jstree'
 ], function ($, Element) {
     'use strict';
 
-    return Element.extend({ 
+    return Element.extend({
         defaults: {
             directoryListUrl: 'media_gallery/directories/getdirectorytreedata',
-            directoryTreeSelector: '#media-gallery-directory-tree'
+            directoryTreeSelector: '#media-gallery-directory-tree',
+            options: {
+                treeInitData: []
+            }
         },
 
-
         /**
-         * Initializes Media Gallery Directories Tree component 
+         * Initializes Media Gallery Directories Tree component
          */
         initialize: function () {
             this._super()
 
             this.createTree();
+
             return this;
         },
 
         /**
-         * Initialize directory tree. 
+         * Get json data for jstree
+         */
+        getJsonTree: function () {
+            $.ajax({
+                url: this.directoryListUrl,
+                type: 'POST',
+                dataType: 'json',
+                async: false,
+                data: {
+                    'form_key': FORM_KEY
+                },
+                success: function (data) {
+                    this.options.treeInitData = data;
+                }.bind(this),
+                error: function (jqXHR, textStatus, errorThrown) {
+                    console.log(textStatus);
+                }.bind(this)
+            });
+        },
+
+        /**
+         * Initialize directory tree.
          */
         createTree: function () {
-            $(this.directoryTreeSelector).folderTree({
-                rootName: 'Root',
-                url: this.directoryListUrl,
-                currentPath: ['root']
+            this.getJsonTree();
+            $(this.directoryTreeSelector).jstree({
+                plugins: ['themes', 'json_data', 'ui', 'crrm', 'types', 'hotkeys'],
+                vcheckbox: {
+                    'two_state': true,
+                    'real_checkboxes': true
+                },
+                'json_data': {
+                    data: this.options.treeInitData
+                },
+                ui: {
+                    'select_limit': 0
+                },
+                hotkeys: {
+                    space: this._changeState,
+                    'return': this._changeState
+                },
+                types: {
+                    'types': {
+                        'disabled': {
+                            'check_node': false,
+                            'uncheck_node': false
+                        }
+                    }
+                }
             });
-            $(this.directoryTreeSelector).bind("loaded.jstree",
-                function (event, data) {
-                    $("a:contains('Root')").css("visibility","hidden")
-                    $(".jstree-last .jstree-icon").first().hide()
-                });
-  
-        },
+        }
     });
-
 
 });
