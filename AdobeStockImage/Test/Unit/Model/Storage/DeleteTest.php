3e458242cc20974432bc46924840a8220056933c
Nazar Klovanych <nazarn96@gmail.com>
2020-02-03 16:19:55 +0200

    refactor unit test

--- AdobeStockImage/Test/Unit/Model/Storage/DeleteTest.php
+++ AdobeStockImage/Test/Unit/Model/Storage/DeleteTest.php
@@ -8,6 +8,8 @@ declare(strict_types=1);
 namespace Magento\AdobeStockImage\Test\Unit\Model\Storage;
 
 use Exception;
+use Magento\AdobeStockImage\Model\Storage\Delete;
+use Magento\Cms\Model\Wysiwyg\Images\Storage;
 use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\Exception\CouldNotDeleteException;
 use Magento\Framework\Filesystem;
@@ -15,7 +17,6 @@ use Magento\Framework\Filesystem\Directory\Write;
 use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Magento\AdobeStockImage\Model\Storage\Delete;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -24,7 +25,7 @@ use Psr\Log\LoggerInterface;
 class DeleteTest extends TestCase
 {
     /**
-     * @var MockObject | Write
+     * @var MockObject|Write
      */
     private $mediaDirectoryMock;
 
@@ -34,12 +35,17 @@ class DeleteTest extends TestCase
     private $delete;
 
     /**
-     * @var MockObject | Filesystem
+     * @var MockObject|Filesystem
      */
-    public $fileSystemMock;
+    private $fileSystemMock;
 
     /**
-     * @var MockObject | LoggerInterface
+     * @var MockObject|Storage
+     */
+    private $storageMock;
+
+    /**
+     * @var MockObject|LoggerInterface
      */
     private $logger;
 
@@ -51,68 +57,99 @@ class DeleteTest extends TestCase
         $this->fileSystemMock = $this->createMock(Filesystem::class);
         $this->logger = $this->createMock(LoggerInterface::class);
         $this->mediaDirectoryMock = $this->createMock(Write::class);
+        $this->storageMock = $this->createMock(Storage::class);
 
         $this->delete = (new ObjectManager($this))->getObject(
             Delete::class,
             [
-                'filesystem'   => $this->fileSystemMock,
-                'logger'          => $this->logger,
+                'filesystem' => $this->fileSystemMock,
+                'logger' => $this->logger,
+                'storage' => $this->storageMock
             ]
         );
     }
 
     /**
      * Test storage delete action
+     *
+     * @param string $path
+     * @param string $absolutePath
+     * @dataProvider getPathDataProvider
+     * @return void
      */
-    public function testExecute(): void
+    public function testExecute(string $path, string $absolutePath): void
     {
         $path = 'path';
+        $absolutePath = '/home/instance/path';
 
         $this->fileSystemMock->expects($this->once())
             ->method('getDirectoryWrite')
             ->with(DirectoryList::MEDIA)
             ->willReturn($this->mediaDirectoryMock);
-
         $this->mediaDirectoryMock->expects($this->once())
             ->method('isFile')
             ->with($path)
             ->willReturn(true);
-
         $this->mediaDirectoryMock->expects($this->once())
-            ->method('delete')
+            ->method('getAbsolutePath')
             ->with($path)
+            ->willReturn($absolutePath);
+        $this->storageMock->expects($this->once())
+            ->method('deleteFile')
+            ->with($absolutePath)
             ->willReturn(true);
 
         $this->delete->execute($path);
     }
 
     /**
-     * Assume that delete action will thrown an Exception
+     * Assume that delete action wilyyl thrown an Exception
+     *
+     * @dataProvider getPathDataProvider
+     * @param string $path
+     * @param string $absolutePath
+     * @return void
      */
-    public function testExceptionOnDeleteExecution(): void
+    public function testExceptionOnDeleteExecution(string $path, string $absolutePath): void
     {
-        $path = 'path';
-
         $this->fileSystemMock->expects($this->once())
             ->method('getDirectoryWrite')
             ->with(DirectoryList::MEDIA)
             ->willReturn($this->mediaDirectoryMock);
-
         $this->mediaDirectoryMock->expects($this->once())
             ->method('isFile')
             ->with($path)
             ->willReturn(true);
-
         $this->mediaDirectoryMock->expects($this->once())
-            ->method('delete')
+            ->method('getAbsolutePath')
             ->with($path)
+            ->willReturn($absolutePath);
+        $this->storageMock->expects($this->once())
+            ->method('deleteFile')
+            ->with($absolutePath)
             ->willThrowException(new Exception());
 
         $this->expectException(CouldNotDeleteException::class);
+
         $this->logger->expects($this->once())
             ->method('critical')
             ->willReturnSelf();
 
         $this->delete->execute($path);
+    }
+
+    /**
+     * Path data provider for tests
+     *
+     * @return array
+     */
+    public function getPathDataProvider(): array
+    {
+        return [
+            [
+                'path',
+                '/home/instance/path'
+            ]
+        ];
     }
 }
