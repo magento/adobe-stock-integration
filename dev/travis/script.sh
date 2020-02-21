bf1889ac4d46b0daa5a94f54b4bd9af73b6a543c
Shankar Konar <konar.shankar2013@gmail.com>
2020-02-21 18:29:35 +0530

    phpstan test included for MediaGallery module

--- dev/travis/script.sh
+++ dev/travis/script.sh
@@ -15,11 +15,15 @@ if [ $TEST_SUITE == 'phpstan' ];
 then
     composer require --dev phpstan/phpstan fooman/phpstan-magento2-magic-methods;
     vendor/bin/phpstan analyse -l 2 app/code/Magento/Adobe* -a dev/tests/api-functional/framework/autoload.php;
+    vendor/bin/phpstan analyse -l 2 app/code/Magento/MediaGalleryUi -a dev/tests/api-functional/framework/autoload.php;
+    vendor/bin/phpstan analyse -l 2 app/code/Magento/MediaGalleryUiApi -a dev/tests/api-functional/framework/autoload.php;
 fi
 
 if [ $TEST_SUITE == 'static' ]; then
-    vendor/bin/phpcs --standard=dev/tests/static/framework/Magento/ app/code/Magento/Adobe* app/code/Magento/Media*;
+    vendor/bin/phpcs --standard=dev/tests/static/framework/Magento/ app/code/Magento/Adobe* app/code/Magento/MediaGalleryUi app/code/Magento/MediaGalleryUiApi;
     ! find app/code/Magento/Adobe*/ -type f -name "*.php" -exec grep -L strict_types=1 {} + | grep Adobe;
+    ! find app/code/Magento/MediaGalleryUi/ -type f -name "*.php" -exec grep -L strict_types=1 {} + | grep MediaGalleryUi;
+    ! find app/code/Magento/MediaGalleryUiApi/ -type f -name "*.php" -exec grep -L strict_types=1 {} + | grep MediaGalleryUiApi;
 fi
 
 if [ $TEST_SUITE == 'functional' ]; then
