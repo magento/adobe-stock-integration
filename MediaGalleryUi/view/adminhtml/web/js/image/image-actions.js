665d168bbf669d17849a8a237ac5ed58d637a280
Sergiy Zhovnir <s.zhovnir@atwix.com>
2020-03-31 12:50:52 +0300

    #issue-1051 Added Add Image feature for Media Gallery View Details popup

--- MediaGalleryUi/view/adminhtml/web/js/image/image-actions.js
+++ MediaGalleryUi/view/adminhtml/web/js/image/image-actions.js
@@ -6,14 +6,20 @@
 define([
     'jquery',
     'underscore',
-    'uiElement'
-], function ($, _, Element) {
+    'uiElement',
+    'mage/translate'
+], function ($, _, Element, $t) {
     'use strict';
 
     return Element.extend({
         defaults: {
             modalSelector: '',
-            template: 'Magento_MediaGalleryUi/image/actions'
+            template: 'Magento_MediaGalleryUi/image/actions',
+            targetElementId: null,
+            mediaGalleryImageDetailsName: 'mediaGalleryImageDetails',
+            modules: {
+                mediaGalleryImageDetails: '${ $.mediaGalleryImageDetailsName }'
+            }
         },
 
         /**
@@ -27,6 +33,67 @@ define([
             }
 
             modalElement.modal('closeModal');
+        },
+
+        /**
+         * Add Image
+         */
+        addImage: function () {
+            var targetElement = this.getTargetElement(),
+                imageDetails = this.getImageData();
+
+            if (!targetElement.length) {
+                this.getMediaGalleryModal().closeDialog();
+                throw $t('Target element not found for content update');
+            }
+
+            targetElement.val(imageDetails['image_url'])
+                .data('size', imageDetails.size)
+                .data('mime-type', imageDetails['content_type'])
+                .trigger('change');
+            this.closeModal();
+            this.getMediaGalleryModal().closeDialog();
+            targetElement.focus();
+        },
+
+        /**
+         * Get image data
+         *
+         * @return {Object}
+         */
+        getImageData: function () {
+            if (!this.mediaGalleryImageDetails) {
+                return {};
+            }
+
+            return this.mediaGalleryImageDetails().getImageData();
+        },
+
+        /**
+         * Close media gallery grid modal
+         */
+        closeMediaGalleryGridModal: function () {
+            this.getMediaGalleryModal().closeDialog();
+        },
+
+        /**
+         * Get target element
+         *
+         * @return {HTMLElement}
+         */
+        getTargetElement: function () {
+            var targetElementSelector = "#{targetElementId}".replace('{targetElementId}', this.targetElementId);
+
+            return $(targetElementSelector);
+        },
+
+        /**
+         * Get media gallery modal
+         *
+         * @return {Object}
+         */
+        getMediaGalleryModal: function () {
+            return window.MediabrowserUtility;
         }
     });
 });
