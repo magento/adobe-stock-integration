c0391bd1ecdae23a23f75541121f950e662bdc26
Nazar Klovanych <nazarn96@gmail.com>
2020-03-31 15:52:42 +0300

    static test fix

--- MediaGalleryUi/view/adminhtml/web/js/grid/columns/image/insertImageAction.js
+++ MediaGalleryUi/view/adminhtml/web/js/grid/columns/image/insertImageAction.js
@@ -2,6 +2,8 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
+/* global FORM_KEY, tinyMceEditors */
 define([
     'jquery',
     'wysiwygAdapter'
