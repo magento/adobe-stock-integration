9b17baa17a95882519f4ad462782366108367617
vadim.malesh <engcom-vendorworker-charlie@adobe.com>
2020-04-01 12:37:01 +0300

    covers #457; fixed Licensed label

--- AdobeStockImageAdminUi/Ui/Component/Listing/Columns/UnlicensedOverlay.php
+++ AdobeStockImageAdminUi/Ui/Component/Listing/Columns/UnlicensedOverlay.php
@@ -18,14 +18,15 @@ class UnlicensedOverlay extends Column
      * Prepare Data Source
      *
      * @param array $dataSource
-     *
      * @return array
      */
     public function prepareDataSource(array $dataSource): array
     {
         if (isset($dataSource['data']['items'])) {
             foreach ($dataSource['data']['items'] as & $item) {
-                $item[$this->getData('name')] = isset($item['licensed']) && !$item['licensed'] ? 'Unlicensed' : '';
+                $item[$this->getData('name')] = isset($item['licensed']) && $item['licensed']
+                    ? 'Licensed'
+                    : 'Unlicensed';
             }
         }
 
