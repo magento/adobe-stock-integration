795a88035c15b5ccad73dfa60b75c96062c630bc
Stanislav Idolov <sidolov@magento.com>
2020-04-10 11:40:23 -0500

    magento/adobe-stock-integration#1146: Eliminate grid index and use assets table instead

--- AdobeStockImage/Model/Plugin/AddLicenseDataToAsset.php
+++ AdobeStockImage/Model/Plugin/AddLicenseDataToAsset.php
@@ -8,9 +8,6 @@ declare(strict_types=1);
 
 namespace Magento\AdobeStockImage\Model\Plugin;
 
-use Magento\AdobeStockAssetApi\Api\AssetRepositoryInterface;
-use Magento\Framework\Api\FilterBuilder;
-use Magento\Framework\Api\Search\SearchCriteriaBuilder;
 use Magento\MediaGalleryUi\Ui\Component\Listing\Provider;
 
 /**
@@ -19,70 +16,16 @@ use Magento\MediaGalleryUi\Ui\Component\Listing\Provider;
 class AddLicenseDataToAsset
 {
     /**
-     * @var AssetRepositoryInterface
-     */
-    private $assetRepository;
-
-    /**
-     * @var FilterBuilder
-     */
-    private $filterBuilder;
-
-    /**
-     * @var SearchCriteriaBuilder
-     */
-    private $searchCriteriaBuilder;
-
-    /**
-     * @param AssetRepositoryInterface $assetRepository
-     * @param FilterBuilder $filterBuilder
-     * @param SearchCriteriaBuilder $searchCriteriaBuilder
-     */
-    public function __construct(
-        AssetRepositoryInterface $assetRepository,
-        FilterBuilder $filterBuilder,
-        SearchCriteriaBuilder $searchCriteriaBuilder
-    ) {
-        $this->assetRepository = $assetRepository;
-        $this->filterBuilder = $filterBuilder;
-        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
-    }
-
-    /**
      * Add license data to assets.
      *
      * @param Provider $subject
-     * @param array $result
-     * @return array
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
-    public function afterGetData(
-        Provider $subject,
-        array $result
-    ) : array {
-        $assetIds = array_column($result, 'id');
-
-        $mediaGalleryIdFilter = $this->filterBuilder->setField('media_gallery_id')
-            ->setValue($assetIds)
-            ->setConditionType('in')
-            ->create();
-        $searchCriteria = $this->searchCriteriaBuilder
-            ->addFilter($mediaGalleryIdFilter)
-            ->create();
-
-        $stockAssets = $this->assetRepository->getList($searchCriteria);
-
-        $licensedData = [];
-        foreach ($stockAssets->getItems() as $asset) {
-            $licensedData[$asset->getMediaGalleryId()] = $asset->getIsLicensed();
-        }
-
-        foreach ($result as $key => $item) {
-            if (isset($licensedData[$item['id']])) {
-                $result[$key]['licensed'] = $licensedData[$item['id']];
-            }
-        }
-
-        return $result;
+    public function beforeLoadWithFilter(Provider $subject) : void
+    {
+        $subject->getSelect()->joinLeft(
+            'adobe_stock_asset',
+            'adobe_stock_asset.media_gallery_id = main_table.id',
+            ['is_licensed']
+        );
     }
 }
