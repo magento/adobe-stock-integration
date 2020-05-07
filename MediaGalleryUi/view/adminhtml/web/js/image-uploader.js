0c5b28f236fdcc3ee2d79f79c1d7fe4aa6d84bbc
chalov <chalov.anton@gmail.com>
2020-05-07 12:36:27 +0300

    fix typo. file from small letter

--- MediaGalleryUi/view/adminhtml/web/js/image-uploader.js
+++ MediaGalleryUi/view/adminhtml/web/js/image-uploader.js
@@ -87,7 +87,7 @@ define([
                         this.mediaGridMessages().add(
                             'error',
                             $.mage.__('Cannot upload <b>' + data.files[0].name +
-                                      '</b>. file exceeds maximum file size limit.')
+                                      '</b>. File exceeds maximum file size limit.')
                         );
 
                         return;
