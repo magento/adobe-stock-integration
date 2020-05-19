482d7d7ce8a1ec9760957e9ed46bcb10b86d333e
Nazar Klovanych <nazarn96@gmail.com>
2020-05-19 12:40:44 +0300

    Fix "Image cannot be located" message appears if try to Save Preview in "Storage Root" folder. The image is saved into new folder named "Undefined"

--- AdobeStockImageAdminUi/view/adminhtml/web/js/components/grid/column/preview/actions.js
+++ AdobeStockImageAdminUi/view/adminhtml/web/js/components/grid/column/preview/actions.js
@@ -340,7 +340,7 @@ define([
             if (this.isMediaBrowser()) {
                 activeNode = this.getMageMediaBrowserData().activeNode;
 
-                activeNodePath = $('[data-id="' + activeNode.id + '"]').length === 0 ? '' : activeNode.path;
+                activeNodePath = $('[data-id="' + activeNode.id + '"]').length === 0 || activeNode.id === 'root' ? '' : activeNode.path;
             } else {
                 activeNodePath = this.imageDirectory().activeNode() || '';
             }
