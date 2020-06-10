1da79abf7cba723630032d87db0ff62298d0adc6
Nazar Klovanych <nazarn96@gmail.com>
2020-06-10 16:51:08 +0300

    Fix  Open Images uploaded outside of media gallery are not reflected in the media gallery

---
+++ MediaGalleryIntegration/Plugin/SaveBaseCategoryImageInformation.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\MediaGalleryIntegration\Plugin;
+
+use Magento\MediaGalleryApi\Api\SaveAssetsInterface;
+use Magento\Catalog\Model\ImageUploader;
+use Magento\MediaGallerySynchronization\Model\CreateAssetFromFile;
+use Magento\Cms\Model\Wysiwyg\Images\Storage;
+use Magento\MediaGalleryUi\Model\Filesystem\SplFileInfoFactory;
+
+/**
+ * Save base category image by SaveAssetsInterface.
+ */
+class SaveBaseCategoryImageInformation
+{
+    /**
+     * @var SplFileInfoFactory
+     */
+    private $splFileInfoFactory;
+    
+    /**
+     * @var SaveAssetsInterface
+     */
+    private $saveAsset;
+
+    /**
+     * @var CreateAssetFromFile
+     */
+    private $createAssetFromFile;
+
+    /**
+     * @var Storage
+     */
+    private $storage;
+
+    /**
+     * @param SplFileInfoFactory $splFileInfoFactory
+     * @param Storage $storage
+     * @param CreateAssetFromFile $createAssetFromFile
+     * @param SaveAssetsInterface $saveAsset
+     */
+    public function __construct(
+        SplFileInfoFactory $splFileInfoFactory,
+        CreateAssetFromFile $createAssetFromFile,
+        SaveAssetsInterface $saveAsset,
+        Storage $storage
+    ) {
+        $this->splFileInfoFactory = $splFileInfoFactory;
+        $this->createAssetFromFile = $createAssetFromFile;
+        $this->saveAsset = $saveAsset;
+        $this->storage = $storage;
+    }
+
+    /**
+     * Save base category image information after movinf from tmp folder.
+     *
+     * @param ImageUploader $subject
+     * @param string $imagePath
+     */
+    public function afterMoveFileFromTmp(ImageUploader $subject, string $imagePath): string
+    {
+        $absolutePath = $this->storage->getCmsWysiwygImages()->getStorageRoot() . $imagePath;
+        $file = $this->splFileInfoFactory->create($absolutePath);
+        $this->saveAsset->execute([$this->createAssetFromFile->execute($file)]);
+        $this->storage->resizeFile($absolutePath);
+
+        return $imagePath;
+    }
+}
