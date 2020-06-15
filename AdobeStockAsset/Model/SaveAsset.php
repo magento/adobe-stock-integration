8c6f8f103e09619e7d4511840350eb5e7643522f
Lena Orobei <oorobei@magento.com>
2020-06-15 15:59:54 -0500

    Syntax error

--- AdobeStockAsset/Model/SaveAsset.php
+++ AdobeStockAsset/Model/SaveAsset.php
@@ -43,7 +43,7 @@ class SaveAsset implements SaveAssetInterface
     /**
      * @var DataObjectProcessor
      */
-    private $objectProcessor;
+    private objectProcessor;
 
     /**
      * @var AssetInterfaceFactory
