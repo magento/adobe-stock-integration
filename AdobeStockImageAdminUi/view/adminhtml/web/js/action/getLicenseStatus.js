a9d4ca92c8a7e6e83d9ab54855607f074fc92a0e
Nazar Klovanych <nazarn96@gmail.com>
2020-06-15 13:15:48 +0300

    Correct error handling

--- AdobeStockImageAdminUi/view/adminhtml/web/js/action/getLicenseStatus.js
+++ AdobeStockImageAdminUi/view/adminhtml/web/js/action/getLicenseStatus.js
@@ -33,7 +33,13 @@ define([
              * @param {Object} response
              */
             error: function (response) {
-                deferred.reject(response.message);
+                var message = response.message;
+
+                if (response.status === 403) {
+                    message = $.mage.__('Your admin role does not have permissions to license an image');
+                }
+
+                deferred.reject(message);
             }
         });
 
