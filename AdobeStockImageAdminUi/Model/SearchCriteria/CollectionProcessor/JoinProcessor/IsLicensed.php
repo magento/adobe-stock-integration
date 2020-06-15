d8c07b5bd8d76e9b5258efe3bc8930eb2821fcf8
Gabriel Galvao da Gama <galvaoda@adobe.com>
2020-06-15 15:05:11 +0100

    Removed SelectModifierInterface and added CollectionProcessor to Data Provider

--- AdobeStockImageAdminUi/Model/SearchCriteria/CollectionProcessor/JoinProcessor/IsLicensed.php
+++ AdobeStockImageAdminUi/Model/SearchCriteria/CollectionProcessor/JoinProcessor/IsLicensed.php
@@ -3,20 +3,15 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-
 declare(strict_types=1);
 
-namespace Magento\AdobeStockImageAdminUi\Model\FilterParametersProvider;
+namespace Magento\AdobeStockImageAdminUi\Model\SearchCriteria\CollectionProcessor\JoinProcessor;
 
-use Magento\Framework\Api\SearchCriteriaInterface;
-use Magento\Framework\DB\Select;
-use Magento\MediaGalleryUi\Model\SelectModifierInterface;
+use Magento\Framework\Api\SearchCriteria\CollectionProcessor\JoinProcessor\CustomJoinInterface;
 use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\Data\Collection\AbstractDb;
 
-/**
- * Add license state
- */
-class IsLicensed implements SelectModifierInterface
+class IsLicensed implements CustomJoinInterface
 {
     private const ADOBE_STOCK_ASSET_TABLE_NAME = 'adobe_stock_asset';
 
@@ -32,16 +27,17 @@ class IsLicensed implements SelectModifierInterface
     {
         $this->connection = $connection;
     }
-    
+
     /**
-     * @inheritdoc
+     * @inheritDoc
      */
-    public function apply(Select $select, SearchCriteriaInterface $searchCriteria): void
+    public function apply(AbstractDb $collection)
     {
-        $select->joinLeft(
+        $collection->getSelect()->joinLeft(
             ['asa' => $this->connection->getTableName(self::ADOBE_STOCK_ASSET_TABLE_NAME)],
             'asa.media_gallery_id = main_table.id',
             ['is_licensed']
         );
     }
+
 }
