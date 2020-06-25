227ec0fffe2223df14a7b731265af3fb37fe9b2d
gregbalonzo <gregbalonzo@gmail.com>
2020-06-25 21:17:58 +0800

    magento/adobe-stock-integration#1462: Introduce Used In filter - Add Used In filter to the media gallery to allow the user to filter by the area where the images are used in: pages, categories, blocks, products

---
+++ AdobeStockImageAdminUi/Model/SearchCriteria/CollectionProcessor/JoinProcessor/EntityType.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\AdobeStockImageAdminUi\Model\SearchCriteria\CollectionProcessor\JoinProcessor;
+
+use Magento\Framework\Api\SearchCriteria\CollectionProcessor\JoinProcessor\CustomJoinInterface;
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\Data\Collection\AbstractDb;
+
+class EntityType implements CustomJoinInterface
+{
+    private const MEDIA_CONTENT_ASSET_TABLE_NAME = 'media_content_asset';
+
+    /**
+     * @var ResourceConnection
+     */
+    private $connection;
+
+    /**
+     * @param ResourceConnection $connection
+     */
+    public function __construct(ResourceConnection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function apply(AbstractDb $collection): bool
+    {
+        $collection->getSelect()->joinLeft(
+            ['mca' => $this->connection->getTableName(self::MEDIA_CONTENT_ASSET_TABLE_NAME)],
+            'mca.asset_id = main_table.id',
+            ['entity_type']
+        );
+
+        return true;
+    }
+}
---
+++ AdobeStockImageAdminUi/Ui/Component/Listing/Columns/Layout/Options.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\AdobeStockImageAdminUi\Ui\Component\Listing\Columns\Layout;
+
+use Magento\Framework\Data\OptionSourceInterface;
+use Magento\Framework\Option\ArrayInterface;
+
+/**
+ * Image source filter options
+ */
+class Options implements ArrayInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function toOptionArray(): array
+    {
+        return [
+            [
+                'value' => 'cms_page',
+                'label' =>  __('Pages'),
+            ],
+            [
+                'value' => 'catalog_category',
+                'label' =>  __('Categories'),
+            ],
+            [
+                'value' => 'cms_block',
+                'label' =>  __('Blocks'),
+            ],
+            [
+                'value' => 'catalog_product',
+                'label' =>  __('Products'),
+            ],
+        ];
+    }
+}
