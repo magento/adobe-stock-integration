f9c849337b0731c2e98e02c9bfa5bd35d60dc9ad
Gabriel Galvao da Gama <galvaoda@adobe.com>
2020-07-16 11:47:42 +0100

    Introduced the MediaGalleryContentUi

---
+++ MediaGalleryContentUi/Model/SearchCriteria/CollectionProcessor/FilterProcessor/ContentField.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\MediaGalleryContentUi\Model\SearchCriteria\CollectionProcessor\FilterProcessor;
+
+use Magento\Framework\Api\Filter;
+use Magento\Framework\Api\SearchCriteria\CollectionProcessor\FilterProcessor\CustomFilterInterface;
+use Magento\Framework\Data\Collection\AbstractDb;
+use Magento\MediaContentApi\Api\GetAssetIdsByContentFieldInterface;
+
+/**
+ * Class responsible to filter a content field
+ */
+class ContentField implements CustomFilterInterface
+{
+    private const TABLE_ALIAS = 'main_table';
+
+    /**
+     * @var GetAssetIdsByContentFieldInterface
+     */
+    private $getAssetIdsByContentStatus;
+
+    /**
+     * ContentField constructor.
+     *
+     * @param GetAssetIdsByContentFieldInterface $getAssetIdsByContentStatus
+     */
+    public function __construct(
+        GetAssetIdsByContentFieldInterface $getAssetIdsByContentStatus
+    ) {
+        $this->getAssetIdsByContentStatus = $getAssetIdsByContentStatus;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function apply(Filter $filter, AbstractDb $collection): bool
+    {
+        $collection->addFieldToFilter(
+            self::TABLE_ALIAS . '.id',
+            ['in' => $this->getAssetIdsByContentStatus->execute($filter->getField(), $filter->getValue())]
+        );
+
+        return true;
+    }
+
+}
