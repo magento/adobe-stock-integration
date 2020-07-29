24ae0ebb9f2f261829fec3d16ec5e596b5b9ab20
Nazar Klovanych <nazarn96@gmail.com>
2020-07-29 17:59:11 +0300

    Unskip tests

--- MediaGalleryMetadata/Test/Integration/Model/ExtractMetadataTest.php
+++ MediaGalleryMetadata/Test/Integration/Model/ExtractMetadataTest.php
@@ -62,33 +62,42 @@ class ExtractMetadataTest extends TestCase
     public function filesProvider(): array
     {
         return [
-            //    [
-            //  'macos-photos.jpeg',
-            //  'Title of the magento image',
-            //  'Description of the magento image',
-            //  [
-            //      'magento',
-            //      'mediagallerymetadata'
-            //  ]
-            // ],
-            // [
-            //  'macos-preview.png',
-            //  'Title of the magento image',
-            //  'Description of the magento image',
-            //  [
-            //      'magento',
-            //      'mediagallerymetadata'
-            //  ]
-            //],
-            //[
-            //  'iptc_only.jpeg',
-            //  'Title of the magento image',
-            //  'Description of the magento image',
-            //  [
-            //      'magento',
-            //      'mediagallerymetadata'
-            //  ]
-            //],
+            [
+                'macos-photos.jpeg',
+                'Title of the magento image',
+                'Description of the magento image',
+                [
+                    'magento',
+                    'mediagallerymetadata'
+                ]
+            ],
+            [
+                'macos-preview.png',
+                'Title of the magento image',
+                'Description of the magento image',
+                [
+                    'magento',
+                    'mediagallerymetadata'
+                ]
+            ],
+            [
+                'iptc_only.jpeg',
+                'Title of the magento image',
+                'Description of the magento image',
+                [
+                    'magento',
+                    'mediagallerymetadata'
+                ]
+            ],
+            [
+                'exiftool.gif',
+                'Title of the magento image',
+                'Description of the magento image',
+                [
+                    'magento',
+                    'mediagallerymetadata'
+                ]
+            ],
             [
                 'iptc_only.png',
                 'Title of the magento image',
@@ -98,15 +107,6 @@ class ExtractMetadataTest extends TestCase
                     'awesome'
                 ]
             ],
-            //[
-            //  'exiftool.gif',
-            //  'Title of the magento image',
-            //  'Description of the magento image',
-            //  [
-            //      'magento',
-            //      'mediagallerymetadata'
-            //  ]
-            //]
         ];
     }
 }
