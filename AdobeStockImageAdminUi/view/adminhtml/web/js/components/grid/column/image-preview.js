a7cbe9e7854d7a6faf49f2874dd8760ea4c02231
Nazar Klovanych <nazarn96@gmail.com>
2020-08-20 16:22:54 +0300

    Hide record on directory deletiion or file deletion

--- AdobeStockImageAdminUi/view/adminhtml/web/js/components/grid/column/image-preview.js
+++ AdobeStockImageAdminUi/view/adminhtml/web/js/components/grid/column/image-preview.js
@@ -61,8 +61,14 @@ define([
          */
         initialize: function () {
             this._super().initView();
-            $(window).on('fileDeleted.enhancedMediaGallery', this.reloadAdobeGrid.bind(this));
-            $(window).on('folderDeleted.enhancedMediaGallery', this.reloadAdobeGrid.bind(this));
+            $(window).on('fileDeleted.enhancedMediaGallery', function () {
+                this.reloadAdobeGrid();
+                this.hide();
+            }.bind(this));
+            $(window).on('folderDeleted.enhancedMediaGallery', function () {
+                this.reloadAdobeGrid();
+                this.hide();
+            }.bind(this));
 
             return this;
         },
